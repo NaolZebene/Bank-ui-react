a1518b2075ff8f81bab6eeacb3110fbc66fbb1da
NaolZebene <zebenenaol@gmail.com>
2022-12-18 21:37:01 +0300

    first init

---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import Navbar from "./Navbar"
+import CTA from "./CTA";
+import Clients from "./Clients";
+import Stats from "./Stats"
+import Footer from "./Footer";
+import Testimonials from "./Testimonials"
+import Hero from "./Hero";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Billing from "./Billing";
+
+
+
+
+export {
+    Navbar,
+    CTA,
+    Clients,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    Billing,
+    CardDeal,
+    Business
+}
